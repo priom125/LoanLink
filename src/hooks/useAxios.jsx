4576612edeb54419f33e6a6cedb3fa1cade40a41
priom125 <priom6046@gmail.com>
2025-12-19 14:48:49 +0600

    secure start

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,10 +1,34 @@
 import axios from 'axios';
+import { useContext, useEffect } from 'react';
+import { AuthContext } from '../Auth/AuthProvider';
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:3000/', // Replace with your API base URL
   timeout: 10000, // Request timeout
 });
 
 const useAxios = () => {
+const {user} = useContext(AuthContext)
+  useEffect(() => {
+    const reqIntercepttor = axiosInstance.interceptors.request.use(config => {
+      config.headers.Authorization = `Bearer ${user?.accessToken}`
+      return config
+    } )
+
+    const resInterceptor = axiosInstance.interceptors.request.use((response)=>{
+      return response
+    },
+    (error) =>{
+      console.log(error);
+      return Promise.reject(error);
+    })
+
+    return () => {
+      axiosInstance.interceptors.request.eject(reqIntercepttor);
+      axiosInstance.interceptors.request.eject(resInterceptor);
+      
+    }
+
+  },[user])
     return axiosInstance;
 };
 export default useAxios;
